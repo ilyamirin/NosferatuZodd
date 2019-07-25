5d401b82e09bc946130b843b100bfa3e72bb5571
alexiskhb <alexiskhb@gmail.com>
2019-07-25 23:53:08 +1000

    Поправил скрипт

--- scripts/xnix/NosferatuZodd-tcp.sh
+++ scripts/xnix/NosferatuZodd-tcp.sh
@@ -2,6 +2,5 @@
 
 clear
 
-script_dir="${0%/*}"
-python3 -m programy.clients.events.tcpsocket.client --config $script_dir/../../config/xnix/config.tcp.yaml --cformat yaml --logging $script_dir/../../config/xnix/logging.yaml
+python3 -m programy.clients.events.tcpsocket.client --config ../../config/xnix/config.tcp.yaml --cformat yaml --logging ../../config/xnix/logging.yaml
 
