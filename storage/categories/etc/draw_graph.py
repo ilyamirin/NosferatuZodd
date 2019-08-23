a76080fe4dbc11c8711efa9db52234c3c3afeb59
MNIXenus <xenus150694@gmail.com>
2019-08-23 18:22:45 +1000

    add graph drawer

---
+++ storage/categories/etc/draw_graph.py
@@ -0,0 +1,64 @@
+from graphviz import Graph
+import os
+import xml.etree.cElementTree as ET
+
+graph = Graph('G', filename='sb_first.gv', engine='dot', format="svg")
+graph.clear()
+graph.attr(bgcolor='white', label='agraph', fontcolor='white', splines="ortho")
+graph.attr('node', fixedsize = "false", shape='box')
+
+topics = []
+edges = []
+home = "C:/Users/Xenus/Desktop/Projects/NosferatuZodd/storage/categories"
+raw_aiml = ''
+
+for aimls in list(os.walk(home))[0][2]:
+    tree = ET.ElementTree(file = home+'/'+aimls)
+    for topic in tree.findall('topic'):
+        topics.append(topic.attrib['name'])
+        for child in topic.getiterator():
+            if child.tag == 'set' and child.attrib['name'] == 'topic':
+                edges.append((topic.attrib['name'], child.text))
+                
+topic_names = ['Страна',
+              'Точная страна',
+              'Сроки страховки',
+              'Пакеты страховки',
+              'Партнерские услуги',
+              'Медицинские услуги',
+              'TV-медицина',
+              'Второе экспертное мнение',
+              'Опасные заболевания',
+              'Priority Pass',
+              'Карты',
+              'Условия',
+              'Управление',
+              'Сбербанк Первый',
+              'Кредит без поручителя',
+              'Кредит с поручителем',
+              'Вклады',
+              'Лидер Сохраняй',
+              'Лидер Пополняй',
+              'Лидер Управляй',
+              'Облигации',
+              'Сбербанк Премьер',
+              'Вклады',
+              'Особый Сохраняй',
+              'Особый Пополняй',
+              'Особый Управляй',
+              'Сейф',
+              'Сберта',
+              'Инвестиции',
+              'Три шага до инвестора',
+              'Риск Профиль',
+              'Инвестиции доходчиво',
+              'Сбербанк инвестор',
+              'Простые инвестиции',
+              'Управление активами',]
+
+topic_list = zip(topics, topic_names)
+for uid, name in topic_list:
+    graph.node(name = uid, id = uid, label = name)
+for ancestor, descendant in edges:
+    graph.edge(ancestor, descendant)
+graph.view()
